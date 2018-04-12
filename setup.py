7b612aaf38a5b850256c99ee2779c2e8309494f8
stephenhky <stephenhky@yahoo.com.hk>
2018-04-12 17:37:06 -0400

    added MANIFEST.in in order to compile properly

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def readme():
 
 
 setup(name='graphflow',
-      version="0.1.0",
+      version="0.1.1",
       description="Algorithms for Graph Flow Analysis",
       long_description="Numerical routines for analyzing data represented by graphs",
       classifiers=[
