5459f69bfafa48708636f1dbf4c11d2062c8b779
stephenhky <stephenhky@yahoo.com.hk>
2018-06-27 14:17:57 -0400

    releasing 0.1.4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def readme():
 
 
 setup(name='graphflow',
-      version="0.1.3",
+      version="0.1.4",
       description="Algorithms for Graph Flow Analysis",
       long_description="Numerical routines for analyzing data represented by graphs",
       classifiers=[
