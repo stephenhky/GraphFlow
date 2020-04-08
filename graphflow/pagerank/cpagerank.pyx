36c0c602962e1ccbed2d09e2c00146a12d9a4e05
Kwan-yuet Ho <stephenhky@yahoo.com.hk>
2020-04-08 17:52:02 -0400

    Cython variable definition bug

--- graphflow/pagerank/cpagerank.pyx
+++ graphflow/pagerank/cpagerank.pyx
@@ -6,11 +6,12 @@ cimport numpy as np
 def pagerank_cython(np.array adjMatrix, dict nodes, float eps, int maxstep):
     cdef int nbnodes = adjMatrix.shape[0]
     cdef np.array r = np.transpose([np.repeat(1 / float(nbnodes), nbnodes)])
+    cdef np.array newr = np.transpose([np.repeat(1 / float(nbnodes), nbnodes)])
     cdef bool converged = False
     cdef int stepid = 0
 
     while not converged and stepid < maxstep:
-        cdef np.array newr = np.matmul(adjMatrix, r)
+        newr = np.matmul(adjMatrix, r)
         converged = np.sum(np.abs(newr - r)) < eps
         r = newr
         stepid += 1
