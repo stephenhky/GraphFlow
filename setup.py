743c226975e9cedc77a2c52bd2bd6f9252f898ba
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2023-12-10 19:35:43 -0500

    initiation of the development of release 0.6.1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def install_requirements():
 
 
 setup(name='graphflow',
-      version="0.6.0",
+      version="0.6.1a1",
       description="Algorithms for Graph Flow Analysis",
       long_description="Numerical routines for analyzing data represented by graphs",
       classifiers=[
