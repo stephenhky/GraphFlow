e69e73990d0ad53a5e617c15f6866651286b64d9
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2024-07-21 18:43:31 -0400

    name updated for US naturalization

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -49,8 +49,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'GraphFlow'
-copyright = u'2018, Kwan-Yuet Ho'
-author = u'Kwan-Yuet Ho'
+copyright = u'2018, Kwan Yuet Stephen Ho'
+author = u'Kwan Yuet Stephen Ho'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
