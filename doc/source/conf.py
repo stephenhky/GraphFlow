492f9a3a4ea9d840ba6a995d60112279b70eadde
stephenhky <stephenhky@yahoo.com.hk>
2025-08-15 10:38:06 -0400

    added absolute path

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -14,6 +14,7 @@
 
 import sys
 import os
+sys.path.insert(0, os.path.abspath('../../src'))
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
