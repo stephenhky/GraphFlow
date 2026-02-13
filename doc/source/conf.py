cacaf02ce2a314ad47ccdf7787089a5b11a83bee
Kwan Yuet Stephen Ho <3810067+stephenhky@users.noreply.github.com>
2026-02-12 23:41:11 -0500

    updated version number for readthedocs

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -47,9 +47,9 @@ author = u'Kwan Yuet Stephen Ho'
 # built documents.
 #
 # The short X.Y version.
-version = u'0.7'
+version = u'0.8'
 # The full version, including alpha/beta/rc tags.
-release = u'0.7.0'
+release = u'0.8.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
