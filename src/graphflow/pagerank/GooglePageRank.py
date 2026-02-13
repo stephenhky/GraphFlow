8f8bff8c224fef306779e5c1615cc56e275be449
Kwan Yuet Stephen Ho <3810067+stephenhky@users.noreply.github.com>
2026-02-12 23:44:14 -0500

    proper array initialization

--- src/graphflow/pagerank/GooglePageRank.py
+++ src/graphflow/pagerank/GooglePageRank.py
@@ -48,7 +48,7 @@ def _calculate_pagerank_matrix(
         maxstep: int=1000
 ) -> Annotated[NDArray[np.float64], Literal["1D Array"]]:
     nbnodes = adjMatrix.shape[0]
-    r = np.transpose([np.repeat(1 / nbnodes, nbnodes)])
+    r = np.transpose(np.array([np.repeat(1 / nbnodes, nbnodes)]))
     converged = False
     stepid = 0
     while not converged and stepid < maxstep:
